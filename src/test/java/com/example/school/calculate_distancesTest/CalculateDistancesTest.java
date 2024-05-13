7c4ecdd00d894e88a7952d64cb0445f7f9702c76
한상희 <tkdgml822@gmail.com>
2024-05-14 01:48:02 +0900

    위도 경도 계산 테스트

---
+++ src/test/java/com/example/school/calculate_distancesTest/CalculateDistancesTest.java
@@ -0,0 +1,16 @@
+package com.example.school.calculate_distancesTest;
+
+import com.example.school.calculate_distances.CalculateDistances;
+import org.junit.jupiter.api.Test;
+
+import static com.example.school.calculate_distances.CalculateDistances.distanceInKilometerByHaversine;
+
+public class CalculateDistancesTest {
+
+    @Test
+    public void 거리계산() {
+        double result = distanceInKilometerByHaversine(35.85406277, 128.6028415, 35.846875, 128.582727);
+        System.out.println("거리계산:" + result);
+
+    }
+}
