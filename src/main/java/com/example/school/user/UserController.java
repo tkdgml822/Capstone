5b81b88d4ff3d1ed14bdcc03b72447479e2bfac1
한상희 <tkdgml822@gmail.com>
2024-05-31 14:14:24 +0900

    User Controller 저장 및 로그인

---
+++ src/main/java/com/example/school/user/UserController.java
@@ -0,0 +1,34 @@
+package com.example.school.user;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/get-all")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/save")
+    public User save(@RequestBody User user) { // @RequestBody로 data 받아옴
+        return userService.save(user);
+    }
+
+    @PostMapping("/login")
+    public boolean login(@RequestBody User user) {
+        return userService.login(user);
+    }
+}
