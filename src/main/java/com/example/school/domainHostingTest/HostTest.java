3e0eac1e26ee9a0abafd0a012690c7c2d658ecd1
한상희 <tkdgml822@gmail.com>
2024-05-31 14:12:56 +0900

    첮 화면 연결 테스트

--- src/main/java/com/example/school/domainHostingTest/HostTest.java
+++ src/main/java/com/example/school/domainHostingTest/HostTest.java
@@ -1,15 +1,21 @@
 package com.example.school.domainHostingTest;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class HostTest {
+
+
     @GetMapping("/")
     @ResponseBody
     public String Test() {
-        return "Test";
+        return "테스트";
     }
 
 }
