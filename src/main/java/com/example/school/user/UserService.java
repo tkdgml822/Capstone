b48557ff19e8f894f9b6920275cd62cf50f90f75
한상희 <tkdgml822@gmail.com>
2024-06-03 20:19:37 +0900

    회원가입 중복 처리, 유저 랭킹 처리

--- src/main/java/com/example/school/user/UserService.java
+++ src/main/java/com/example/school/user/UserService.java
@@ -1,27 +1,25 @@
 package com.example.school.user;
 
 import com.example.school.rank.RankDTO;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
 
-    @Autowired
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-
     // 회원가입
-    public User save(User user){
+    public User save(User user) {
         if (user.getRankPoint() == null) {
             user.setRankPoint(0L);
         }
@@ -30,22 +28,25 @@ public class UserService {
     }
 
     // 모든 유저 반환
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
     // 유저 삭제
-    public void delete(User user){
+    public void delete(User user) {
         userRepository.delete(user);
     }
 
     // 유저 로그인
     public boolean login(User user) {
+        log.info("userId={}", user.getUserID());
+
         Optional<User> findUser = userRepository.findByUserIDAndPassword(user.getUserID(), user.getPassword());
         return findUser.isPresent();
     }
 
-    public List<RankDTO> getTopFiveUserRanking() {
+    // 랭킹 탑 5
+    public List<RankDTO> getUserRankingTopFive() {
         return userRepository.findAll().stream()
                 .map(user -> new RankDTO(user.getUserID(), user.getRankPoint()))
                 .sorted(Comparator.comparingLong(RankDTO::getRankPoint).reversed())
@@ -53,4 +54,31 @@ public class UserService {
                 .collect(Collectors.toList());
     }
 
+    public int getUserRank(String username) {
+        log.info("userid={}", username);
+        User userId = userRepository.findByUserID(username);
+
+        List<User> users = userRepository.findAll();
+        // 여기서 정렬 순서를 반대로 합니다.
+        List<User> sortedUsers = users.stream()
+                .sorted(Comparator.comparingLong(User::getRankPoint).reversed())
+                .toList();
+
+        int resultRank = getResultRank(sortedUsers, userId);
+
+        return resultRank + 1;
+    }
+
+    private static int getResultRank(List<User> sortedUsers, User userId) {
+        return IntStream.range(0, sortedUsers.size())
+                .filter(i -> sortedUsers.get(i).getUserID().equals(userId.getUserID()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저를 찾을 수 없습니다."));
+    }
+
+    // 중복되는 유저가 있으면 false
+    public boolean checkUserExistence(String userId) {
+        Optional<User> user = Optional.ofNullable(userRepository.findByUserID(userId));
+        return user.isEmpty();
+    }
 }
