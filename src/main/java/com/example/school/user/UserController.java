3fd8f3ed0748fad574867f74440d4a4b5c5c4340
한상희 <tkdgml822@gmail.com>
2024-06-03 20:20:46 +0900

    아이디 중복 처리 컨트롤러 및 유저 랭킹 컨트롤러

--- src/main/java/com/example/school/user/UserController.java
+++ src/main/java/com/example/school/user/UserController.java
@@ -2,11 +2,15 @@ package com.example.school.user;
 
 
 import com.example.school.rank.RankDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -24,8 +28,21 @@ public class UserController {
     }
 
     @PostMapping("/save")
-    public User save(@RequestBody User user) { // @RequestBody로 data 받아옴
-        return userService.save(user);
+    public ResponseEntity<?> save(@RequestBody User user) { // @RequestBody로 data 받아옴
+        ResponseEntity<String> CONFLICT = userIdCheck(user);
+        if (CONFLICT != null) return CONFLICT;
+        User savedUser = userService.save(user);
+
+        return ResponseEntity.ok(savedUser);
+    }
+
+    private ResponseEntity<String> userIdCheck(User user) {
+        if (!userService.checkUserExistence(user.getUserID())) {
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .body("아이디가 이미 사용 중입니다.");
+        }
+        return null;
     }
 
     @PostMapping("/login")
@@ -33,8 +50,15 @@ public class UserController {
         return userService.login(user);
     }
 
-    @GetMapping("rank/all")
+    // 전체 랭킹
+    @GetMapping("/rank/all")
     public List<RankDTO> userRankTopFive() {
-        return userService.getTopFiveUserRanking();
+        return userService.getUserRankingTopFive();
     }
+
+    @GetMapping("/rank/user")
+    public Integer usesRank(@RequestParam("userID") String userID) {
+        return userService.getUserRank(userID);
+    }
+    
 }
